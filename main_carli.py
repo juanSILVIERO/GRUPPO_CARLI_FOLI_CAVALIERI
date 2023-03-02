a7803a512b14f98c04a6bf7e2fffbffca9672476
juanSILVIERO <121232029+juanSILVIERO@users.noreply.github.com>
2023-03-02 13:10:54 +0100

    aggiunta file main

---
+++ main_carli.py
@@ -0,0 +1,12 @@
+import main_stefano
+
+def saluto_giovanni_carli():
+    print("Buongiorno, GIOVANNI CARLI")
+
+def main():
+    main_stefano.saluto_stefano_cavalieri()
+    saluto_giovanni_carli()
+
+
+
+main()
